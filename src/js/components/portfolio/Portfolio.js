7844c9143eae5cbfc52725c7ce862b81f8804104
Maksym Kuziv <mkuziv@hubspot.com>
2022-12-20 00:31:51 +0200

    grid template

--- src/js/components/portfolio/Portfolio.js
+++ src/js/components/portfolio/Portfolio.js
@@ -13,18 +13,12 @@ const Portfolio = () => {
                     <div className="portfolio-text Montserrat-400">Our team helped global brands design, build, launch, and scale their digital products! We bring digital transformation across multiple industries (from social, finTech and healthTech to IoT and Transportation)</div>
                 </div>
                 <div className="portfolio-blocks">
-                    <div className="flex-container-block">
                         <Block logo='1' title='1' text={'Crypto'} className='portfolio1'/>
-                        <Block logo='2' title='2' text={'AR, Sport'}className='portfolio2 margin-left-16'/>
-                    </div>
-                    <div className="flex-container-block">
+                        <Block logo='2' title='2' text={'AR, Sport'}className='portfolio2'/>
                         <Block logo='3' title='3' text={'Healthca'} className='portfolio3'/>
-                        <Block logo='4' title='4' text={'NFT'}className='portfolio4 margin-left-16'/>
-                    </div>
-                    <div className="flex-container-block">
+                        <Block logo='4' title='4' text={'NFT'}className='portfolio4'/>
                         <Block logo='5' title='5' text={'Web3'} className='portfolio5'/>
-                        <Add className='margin-left-16'/>
-                    </div>
+                        <Add />
                 </div>
             </div>
         </Container>
