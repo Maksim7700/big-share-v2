b9ac8e160b7a52f8e00b2bb08cc66efffd8fd91d
Maksym Kuziv <mkuziv@hubspot.com>
2022-12-21 00:12:20 +0200

    Added 90% build

--- src/js/components/technology/Technology.js
+++ src/js/components/technology/Technology.js
@@ -8,7 +8,7 @@ const Technology = () => {
         <div className='technologies'>
         <Container>
             <div className="flex-container-technology">
-                <div className="Gilroy-700 technology-title">Creating digital innovations with the<br/> latest <span>technologies</span></div>
+                <div className="Gilroy-700 technology-title">Creating digital innovations with the latest <span>technologies</span></div>
                 <div className="Montserrat-400 technology-text">Our team consists of enthusiastic technologists who love bringing real world solutions to our clients’ problems. Our ideas stem from a deep understanding of our clients’ markets, solutions, and visions.</div>
                 <div className="languages"><Language /></div>
             </div>
