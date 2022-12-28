7956c44725d38276460e496a37a682a30fde71bf
Maksym Kuziv <mkuziv@hubspot.com>
2022-12-28 22:16:57 +0200

    New version 5

--- src/js/components/blog/Article.js
+++ src/js/components/blog/Article.js
@@ -5,7 +5,9 @@ import '../../../css/laptop.css';
 const Article = () => {
   return (
     <div className='flex-container-article'>
-      <div className='UNDER_DEVELOP Gilroy-700'>UNDER DEVELOPMENT</div>
+      <div className='under_dev'>
+      <div className='UNDER_DEVELOP Gilroy-700'>Under development</div>
+      </div>
         <div className='flex-container-left'>
           <div className='article-title Gilroy-500'>JavaScript</div>
           <div className='article-text Montserrat-400'>We estimate professional knowledge and expertise as the most valuable things a company should possess. Our blog is a platform for promoting and sharing those things to make the knowledge work for the goals and for the best purposes.</div>
--- src/js/components/portfolio/Add.js
+++ src/js/components/portfolio/Add.js
@@ -8,7 +8,7 @@ const Add = (props) => {
     <Scroll to='anketa' spy={true} smooth={true}>
       <div className={`portfolio-add-block ${props.className}`}>
         <div className='inside-add'>
-          <div className='add-text Gilroy-500'>Add my project</div>
+          <div className='add-text Montserrat-500'>Add my project</div>
           <div className='add-star-image'><img alt='add' src='./portfolio/add-star.svg'/></div>
         </div>
       </div>
