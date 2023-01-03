20d567ff17e5cb49e2751c9b2ec0669978d8e9fc
Maksym Kuziv <mkuziv@hubspot.com>
2023-01-03 18:27:16 +0200

    TEST

--- src/js/components/head/Head.js
+++ src/js/components/head/Head.js
@@ -9,18 +9,23 @@ import {Link as Scroll} from 'react-scroll';
 const Head = () => {
     return (
         <Container>
-            <div className="flex-containte-head em-02">
-                <div className="text-head-1 Gilroy-700">Let’s Make Software<br/> <span>Together</span></div>
-                <div className="text-head-2 Montserrat-400">Big or small, immediate or ongoing projects, individual or team-based projects: get them all done with Big Share professionals.
-                    Develop your business together with us.</div>
-                <Scroll to='anketa' spy={true} smooth={true}>
-                    <div className="text-head-3">
-                        <button className="get-started-button Montserrat-500">
-                            <div className="get-started">Get Started</div>
-                            <div className="arrow-right-get-started"></div>
-                        </button>
-                    </div>
-                </Scroll>
+            <div className='flex-head-container'>
+                <div className="flex-containte-head em-02">
+                    <div className="text-head-1 Gilroy-700">Let’s Make Software<br/> <span>Together</span></div>
+                    <div className="text-head-2 Montserrat-400">Big or small, immediate or ongoing projects, individual or team-based projects: get them all done with Big Share professionals.
+                        Develop your business together with us.</div>
+                    <Scroll to='anketa' spy={true} smooth={true}>
+                        <div className="text-head-3">
+                            <button className="get-started-button Montserrat-500">
+                                <div className="get-started">Get Started</div>
+                                <div className="arrow-right-get-started"></div>
+                            </button>
+                        </div>
+                    </Scroll>
+                </div>
+                <div className='pos-abs'>
+                    <video className='video' src='./head/v1.mp4' autoPlay loop muted/>
+                </div>
             </div>
         </Container>
     )
