13a9cb14947f9be579daaa722c563e80a3c52268
Maksym Kuziv <mkuziv@hubspot.com>
2023-01-05 01:31:21 +0200

    D

--- src/js/components/anketa/Select.js
+++ src/js/components/anketa/Select.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import '../../../css/anketa/select.css';
 import '../../../css/hover.css';
 import '../../../css/laptop.css';
@@ -6,20 +5,9 @@ import '../../../css/laptop.css';
 const Select = (props) => {
   const image = props.image;
   const title = props.title;
-
-  const [img, setImg] = useState(props.image);
-
-  const changeImage = () => {
-    setImg('complex-service-hover.svg');
-  }
-
-  const changeImage1 = () => {
-    setImg(props.image);
-  }
-
   return (
-    <div onMouseOver={changeImage} onMouseOut={changeImage1} onClick={props.myClick} className={`select select-${props.num}`}>
-      <img alt={`${image}`} src={`/anketa/${img}`}/>
+    <div onClick={props.myClick} className={`select select-${props.num}`}>
+      <img alt={`${image}`} src={`/anketa/${image}`}/>
       <div className='select-title Gilroy-500'>{title}</div>
     </div>
   )
