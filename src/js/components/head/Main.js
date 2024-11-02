dafcc11027d5f0ae6dde26569c6b4442d162e526
Maksym7700 <maxkuziv7@gmail.com>
2024-11-02 13:21:08 +0200

    Added background animation

--- src/js/components/head/Main.js
+++ src/js/components/head/Main.js
@@ -1,13 +1,15 @@
 import '../../../css/head/main.css';
 import Head from "../head/Head";
 import '../../../css/laptop.css';
+import ParticlesComponent from './ParticlesComponent';
 
 const Main = () => {
     return (
         <div className="main">
+            <ParticlesComponent id="particles" />
             <Head />
         </div>
-    )
+    );
 }
 
 export default Main;
